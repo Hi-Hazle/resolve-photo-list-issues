3d8d9db5e7ec52f50198dd42f9d98ba2b7c1bc1e
Hi-Hazle <120439883+Hi-Hazle@users.noreply.github.com>
2024-09-20 11:26:56 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,16 @@ export default function Home() {
   return (
     <>
       <Link href='/refactoring/zustand/rerender'>
-        <Typography variant='h6'>1. Zustand - Rerender</Typography>
+        <Typography variant='h6'>1. Zustand - Rerender : Problem & Solutions</Typography>
       </Link>
       <Link href='/refactoring/react-query/problem-1?id=1'>
-        <Typography variant='h6'>1. ReactQuery - Prefetch & useQuery</Typography>
+        <Typography variant='h6'>2. ReactQuery - Prefetch & useQuery : Problem</Typography>
       </Link>
       <Link href='/refactoring/react-query/solution-1-1?id=1'>
-        <Typography variant='h6'>1. ReactQuery - Prefetch & useQuery</Typography>
+        <Typography variant='h6'>2. ReactQuery - Prefetch & useQuery : Solution1</Typography>
+      </Link>
+      <Link href='/refactoring/react-query/solution-1-2?id=1'>
+        <Typography variant='h6'>2. ReactQuery - Prefetch & useQuery : Solution2</Typography>
       </Link>
     </>
   );
